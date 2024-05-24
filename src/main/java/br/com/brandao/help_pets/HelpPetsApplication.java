81c83193aee4db7374b02218949b433ea20df684
Paulo Brandão <paulo.199706@live.com>
2024-05-23 23:24:57 -0300

    first commit
    Signed-off-by: Paulo Brandão <paulo.199706@live.com>

---
+++ src/main/java/br/com/brandao/help_pets/HelpPetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.brandao.help_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brandao/help_pets/HelpPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brandao.help_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
