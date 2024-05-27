9c7903f4a4a1a738e03f7ea3ece18b373fae6e18
betobrandaojr <59041231+betobrandaojr@users.noreply.github.com>
2024-05-26 23:17:21 -0300

    created user CRUD

---
+++ src/main/java/br/com/brandao/help_pets/module/User.java
@@ -0,0 +1,42 @@
+package br.com.brandao.help_pets.module;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String adress;
+
+    private String contact;
+
+    private String document;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
